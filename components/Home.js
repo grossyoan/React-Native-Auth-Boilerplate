66633224dbe851281fc5d911ae5979322334481f
grossyoan <gross.yoan@gmail.com>
2020-04-07 23:38:50 -0400

    WIP(Database): working on note system

--- components/Home.js
+++ components/Home.js
@@ -1,15 +1,139 @@
 // Main.js
-import React from 'react';
-import {Text, View, Button} from 'react-native';
+import React, {useState} from 'react';
+import {
+  Alert,
+  Text,
+  View,
+  Button,
+  StyleSheet,
+  ScrollView,
+  TextInput,
+} from 'react-native';
 import auth from '@react-native-firebase/auth';
-export default () => {
-  const Signout = () => {
-    auth().signOut();
+import database from '@react-native-firebase/database';
+import CheckBox from 'react-native-check-box';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'white',
+  },
+  contentContainerStyle: {
+    alignItems: 'center',
+  },
+  textInput: {
+    borderWidth: 1,
+    borderColor: '#afafaf',
+    width: '80%',
+    borderRadius: 5,
+    paddingHorizontal: 10,
+    marginVertical: 20,
+    fontSize: 20,
+  },
+  todoItem: {
+    flexDirection: 'row',
+    marginVertical: 10,
+    alignItems: 'center',
+  },
+  todoText: {
+    borderColor: '#afafaf',
+    paddingHorizontal: 5,
+    paddingVertical: 7,
+    borderWidth: 1,
+    borderRadius: 5,
+    marginRight: 10,
+    minWidth: '50%',
+    textAlign: 'center',
+  },
+});
+const DbUserRoot = database().ref('users/' + auth().currentUser.uid + '/notes');
+
+const ToDoItem = ({todoItem: {todoItem: name, done}, id}) => {
+  const [doneState, setDone] = useState(done);
+  const onCheck = () => {
+    setDone(!doneState);
+    DbUserRoot.update({
+      [id]: {
+        todoItem: name,
+        done: !doneState,
+      },
+    });
   };
   return (
-    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-      <Text style={{fontSize: 30, marginBottom: 16}}>Welcome back sir!</Text>
-      <Button title="Disconnect" onPress={Signout} />
+    <View style={styles.todoItem}>
+      <CheckBox
+        checkBoxColor="skyblue"
+        onClick={onCheck}
+        isChecked={doneState}
+        disabled={doneState}
+      />
+      <Text style={[styles.todoText, {opacity: doneState ? 0.2 : 1}]}>
+        {name}
+      </Text>
     </View>
+  );
+};
+
+export default () => {
+  const [todos, setTodos] = useState({});
+  const [presentToDo, setPresentToDo] = useState('');
+  let todosKeys = Object.keys(todos);
+  DbUserRoot.on('value', (snapshot) => {
+    let data = snapshot.val() ? snapshot.val() : {};
+    let todoItems = {...data};
+    setTodos(todoItems);
+  });
+
+  const addNewTodo = () => {
+    if (presentToDo !== '') {
+      DbUserRoot.push().set({
+        done: false,
+        todoItem: presentToDo,
+      });
+      Alert.alert('Action!', 'A new To-do item was created');
+      setPresentToDo('');
+    } else {
+      Alert.alert('You have to write something!');
+    }
+  };
+
+  const clearTodos = () => {
+    DbUserRoot.remove();
+  };
+
+  return (
+    <ScrollView
+      style={styles.container}
+      contentContainerStyle={styles.contentContainerStyle}>
+      <View style={{marginTop: 50}}>
+        {todosKeys.length > 0 ? (
+          todosKeys.map((key) => (
+            <ToDoItem key={key} id={key} todoItem={todos[key]} />
+          ))
+        ) : (
+          <Text>Nothing to see yet</Text>
+        )}
+      </View>
+      <TextInput
+        placeholder="Add new Todo"
+        value={presentToDo}
+        style={styles.textInput}
+        onChangeText={(e) => {
+          setPresentToDo(e);
+        }}
+        onSubmitEditing={addNewTodo}
+      />
+      <Button
+        title="Add new To do item"
+        onPress={addNewTodo}
+        color="lightgreen"
+      />
+      <View style={{marginTop: 20}}>
+        <Button title="Clear todos" onPress={clearTodos} color="red" />
+      </View>
+      <View style={{marginTop: 20}}>
+        <Button title="Sign out" onPress={() => auth().signOut()} color="red" />
+      </View>
+    </ScrollView>
   );
 };
